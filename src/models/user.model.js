6bbc5cd3b4ad7e0b3e112c8441852654753a6f02
Osama4488 <syed.osama.ali.96@gmail.com>
2024-01-24 01:58:38 +0500

    add user controller and fix bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-const userSchema = new Schema.create(
+const userSchema = new Schema(
   {
     username: {
       type: String,
@@ -51,7 +51,7 @@ const userSchema = new Schema.create(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
